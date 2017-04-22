a45634a84ad129da74e44e1126afed71935c2d40
Robert H <robert_heiss@hotmail.com>
2017-04-22 13:58:06 +0200

    C# version in progress Calculation is done

---
+++ BombSightTable/BombSightTableForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BombSightTable
+{
+    partial class BombSightTableForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.inputApproachHeadingMasked = new System.Windows.Forms.MaskedTextBox();
+            this.inputAltitudeMasked = new System.Windows.Forms.MaskedTextBox();
+            this.inputWindHeadingMasked = new System.Windows.Forms.MaskedTextBox();
+            this.inputWindSpeedMasked = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Approach Heading:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Altitude:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wind Heading:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Wind Speed:";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(118, 169);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(68, 23);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // inputApproachHeadingMasked
+            // 
+            this.inputApproachHeadingMasked.Location = new System.Drawing.Point(118, 28);
+            this.inputApproachHeadingMasked.Mask = "000";
+            this.inputApproachHeadingMasked.Name = "inputApproachHeadingMasked";
+            this.inputApproachHeadingMasked.Size = new System.Drawing.Size(68, 20);
+            this.inputApproachHeadingMasked.TabIndex = 10;
+            this.inputApproachHeadingMasked.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
+            // 
+            // inputAltitudeMasked
+            // 
+            this.inputAltitudeMasked.Location = new System.Drawing.Point(118, 66);
+            this.inputAltitudeMasked.Mask = "00000";
+            this.inputAltitudeMasked.Name = "inputAltitudeMasked";
+            this.inputAltitudeMasked.Size = new System.Drawing.Size(68, 20);
+            this.inputAltitudeMasked.TabIndex = 11;
+            this.inputAltitudeMasked.ValidatingType = typeof(int);
+            // 
+            // inputWindHeadingMasked
+            // 
+            this.inputWindHeadingMasked.Location = new System.Drawing.Point(119, 104);
+            this.inputWindHeadingMasked.Mask = "000";
+            this.inputWindHeadingMasked.Name = "inputWindHeadingMasked";
+            this.inputWindHeadingMasked.Size = new System.Drawing.Size(68, 20);
+            this.inputWindHeadingMasked.TabIndex = 12;
+            // 
+            // inputWindSpeedMasked
+            // 
+            this.inputWindSpeedMasked.Location = new System.Drawing.Point(119, 142);
+            this.inputWindSpeedMasked.Mask = "00";
+            this.inputWindSpeedMasked.Name = "inputWindSpeedMasked";
+            this.inputWindSpeedMasked.Size = new System.Drawing.Size(68, 20);
+            this.inputWindSpeedMasked.TabIndex = 13;
+            // 
+            // BombSightTableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 238);
+            this.Controls.Add(this.inputWindSpeedMasked);
+            this.Controls.Add(this.inputWindHeadingMasked);
+            this.Controls.Add(this.inputAltitudeMasked);
+            this.Controls.Add(this.inputApproachHeadingMasked);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BombSightTableForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InputParametersForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.MaskedTextBox inputApproachHeadingMasked;
+        private System.Windows.Forms.MaskedTextBox inputAltitudeMasked;
+        private System.Windows.Forms.MaskedTextBox inputWindHeadingMasked;
+        private System.Windows.Forms.MaskedTextBox inputWindSpeedMasked;
+    }
+}
+
---
+++ BombSightTable/BombSightTableForm.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BombSightTable
+{
+    public partial class BombSightTableForm : Form
+    {
+        public BombSightTableForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            // create empty string to collect the Errors
+            string errorMessages = String.Empty;
+            string fieldFocus = String.Empty;
+
+            // perform the checks and collect the errors
+            if (inputWindSpeedMasked.Text == "")
+            {
+                errorMessages = "Please enter the forecast wind speed!\r\n" + errorMessages;
+                fieldFocus = "4";
+                //inputWindSpeedMasked.Focus();
+                //return;
+            }
+
+            if (inputWindHeadingMasked.Text == "")
+            {
+                errorMessages = "Please enter the forecast wind direction!\r\n" + errorMessages;
+                fieldFocus = "3";
+                //inputWindHeadingMasked.Focus();
+                //return;
+            }
+
+            if (inputAltitudeMasked.Text == "")
+            {
+                errorMessages = "Please enter your desired approach altitude!\r\n" + errorMessages;
+                fieldFocus = "2";
+                //inputAltitudeMasked.Focus();
+                //return;
+            }
+
+            if (inputApproachHeadingMasked.Text == "")
+            {
+                errorMessages = "Please enter your desired target approach heading!\r\n" + errorMessages;
+                fieldFocus = "1";
+                //inputApproachHeadingMasked.Focus();
+                //return;
+            }
+            else
+            {
+                if((Convert.ToInt32(inputApproachHeadingMasked.Text)) > 360)
+                {
+                    errorMessages = "Please enter your desired target approach heading!\r\nThe max value for approach heading is 360!\r\n" + errorMessages;
+                    fieldFocus = "1";
+                    //inputApproachHeadingMasked.Focus();
+                    //return;
+                }
+            }
+
+
+            // debug fieldFocus vaiable
+            if (fieldFocus != "")
+            {
+                MessageBox.Show(fieldFocus);
+            }
+
+            // output collected Errors
+            if (!String.IsNullOrEmpty(errorMessages))
+            {
+                MessageBox.Show(errorMessages, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                
+                // put focus on first error
+                if (fieldFocus == "1")
+                {
+                    ActiveControl = inputApproachHeadingMasked;
+                }
+                if (fieldFocus == "2")
+                {
+                    ActiveControl = inputAltitudeMasked;
+                }
+                if (fieldFocus == "3")
+                {
+                    ActiveControl = inputWindHeadingMasked;
+                }
+                if (fieldFocus == "4")
+                {
+                    ActiveControl = inputWindSpeedMasked;
+                }
+
+            }
+            else
+            {
+                // loop to create a certain range of outputs based on calculated results
+
+                // convert all form strings to int values
+                int acHeading = 0;
+                int acAltitude = 0;
+                int wHeading = 0;
+                int wSpeed = 0;
+
+                acHeading = Int32.Parse(inputApproachHeadingMasked.Text);
+                acAltitude = Int32.Parse(inputAltitudeMasked.Text);
+                wHeading = Int32.Parse(inputWindHeadingMasked.Text);
+                wSpeed = Int32.Parse(inputWindSpeedMasked.Text);
+
+                // define some variables
+                int circle = 360;
+                int correction = 0;
+                string sideCorrection = string.Empty;
+                int i = -10;
+
+                while(i <= 10)
+                {
+                    if(i==-10)
+                    {
+                        acHeading = acHeading + i;
+                        correction = correction + i;
+                    }
+                    else
+                    {
+                        acHeading = acHeading + 1;
+                        correction = correction + 1;
+                    }
+
+                    // correction fro crosing 360
+                    if (correction < 0)
+                    {
+                        correction = 360 + correction;
+                    }
+                    if(acHeading < 0)
+                    {
+                        acHeading = 360 + acHeading;
+                    }
+
+                    // calculate left / right
+                    if (acHeading > wHeading)
+                    {
+                        // substract approach heading from widn heading if result is bigger than 180
+                        correction = (acHeading - wHeading);
+
+                        sideCorrection = "left";
+
+                        if (correction > 180)
+                        {
+                            correction = (circle - acHeading) + wHeading;
+                            sideCorrection = "right";
+                        }
+
+                    }
+                    else
+                    {
+                        //substract  wHeading from acHeading and check if result is bigger then 180
+                        // set offset variable accordingly
+                        correction = wHeading - acHeading;
+
+                        sideCorrection = "right";
+
+                        if (correction > 180)
+                        {
+                            correction = (circle - wHeading) + acHeading;
+                            sideCorrection = "left";
+                        }
+                    }
+
+                    // debug output sideCorrection
+                    MessageBox.Show("sideCorrection:\r\n" + sideCorrection);
+
+                    // debug acHeading
+                    MessageBox.Show("acHeading:\r\n" + acHeading);
+
+                    // debug correction
+                    MessageBox.Show("correction:\r\n" + correction);
+
+                    i = i + 1;
+
+                }
+
+            }
+                
+        }
+
+        private void inputApproachHeading_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
